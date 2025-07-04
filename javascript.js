49443027423fac0f3741f38f34efe05d68a84691
Roberto Angel Serrano Munoz <roserranolc@gmail.com>
2025-07-04 12:07:35 -0600

    Pseudocodigo

---
+++ javascript.js
@@ -0,0 +1,21 @@
+console.log ("Hello World")
+//Crear una variable que retorne prieda papel o tijera
+/* 
+Crear tres variables con los valores piedra papel tjeras
+-Una variable que revuelva esos valores "puno cerrado"
+-Variable que retenga un valor qeu ponga el usuario "competidor"
+de a tres, uno siendo piedra, dos papel y tres tijeras
+-Una variable if que compare si "puno cerrado" es = "competidor".
+Si es igual imprime el resultado ganador
+Si pierde imprime el resultado perdedor*/
+
+const rock = 0;
+const scisors = 2;
+const paper = 3;
+function getComputer (){
+return Math.floor    
+
+
+}
+
+
